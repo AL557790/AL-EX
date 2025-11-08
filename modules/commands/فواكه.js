30bd4108cd57d572627514de2c6f0dcc289454a7
AL EX <fysbwknty@gmail.com>
2025-11-08 14:05:19 +0100

    Update فواكه.js

--- modules/commands/فواكه.js
+++ modules/commands/فواكه.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const fruits = ["🍎", "🍌", "🍒", "🍇", "🍉", "🍓", "🍍", "🍏", "🥝", "🥭", "🫒"];
-const dbPath = path.join(__dirname, "includes", "balances.json");
+const dbPath = path.join(__dirname, "💰Balances", "balances.json");
 const START_BALANCE = 1000;
 
 function loadBalances() {
